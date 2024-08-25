023a5f2f65d50f312bf0b9383554126ee4495028
Mem2019 <r3tr0spect2019@qq.com>
2024-08-24 20:55:30 -0400

    handle main

--- clone.sh
+++ clone.sh
@@ -4,4 +4,6 @@ rm -rf tmp
 git clone https://github.com/lungetech/cgc-challenge-corpus.git tmp
 rm -rf cgc-challenge-corpus && mkdir cgc-challenge-corpus
 cat targets.txt | xargs -I{} mv tmp/{} cgc-challenge-corpus/
+find cgc-challenge-corpus -type f -exec \
+	sed -i 's|main(void)|main(int, char **, char **)|g' {} +
 rm -rf tmp
