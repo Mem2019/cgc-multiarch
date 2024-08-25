65757f83dada6b93db784fe856201e8e87b09695
Mem2019 <r3tr0spect2019@qq.com>
2024-08-24 23:19:22 -0400

    update link

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 set -e
 
 rm -rf tmp
-git clone https://github.com/lungetech/cgc-challenge-corpus.git tmp
+git clone https://github.com/Mem2019/cgc-challenge-corpus.git tmp
 rm -rf cgc-challenge-corpus && mkdir cgc-challenge-corpus
 cat targets.txt | xargs -I{} mv tmp/{} cgc-challenge-corpus/
 find cgc-challenge-corpus -type f -exec \
